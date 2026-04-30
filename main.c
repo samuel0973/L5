d105080eacfe7d6e27ca483193f5c6a816a5cc73
samuel0973 <samuel.bru.mezquita@estudiantat.upc.edu>
2026-04-30 16:29:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,29 @@
 #include "GLCD.h"
 #define _XTAL_FREQ 8000000  
 
+#define BTN_UP 		0x01
+#define BTN_DOWN 	0x02
+#define BTN_RIGHT	0x04
+#define BTN_LEFT	0x08
+#define BTN_SELECT	0x10
+#define BTN_MASK	0x1F
+
 
 int adc_antic = 10000;
 const char * s_nom = "Samuel Mezquita\n";
+
+
+unsigned char read_buttons() {
+   return PORTB & BTN_MASK;
+}
+
+unsigned char debounce_buttons() {
+   unsigned char b1 = read_buttons();
+   __delay_ms(20);
+   unsigned char b2 = read_buttons();
+   
+   return b1 & b2;
+}
 
 
 void writeTxt(byte page, byte y, char * s) {
@@ -24,22 +44,26 @@ void writeTxt(byte page, byte y, char * s) {
 }
 
 show_rentat() {
-   writeTxt(1, 0, "Sabo:\n");
-   writeTxt(3, 0, "Temps:\n");
-   writeTxt(5, 0, "Velocitat:\n");
+   writeTxt(1, 2, "Sabo:\n");
+   writeTxt(3, 2, "Temps:\n");
+   writeTxt(5, 2, "Velocitat:\n");
 }
 
 show_esbandit() {
-   writeTxt(1, 0, "Numero vegades:\n");
-   writeTxt(3, 0, "Temps:\n");
-   writeTxt(5, 0, "Velocitat:\n");
+   writeTxt(1, 2, "Numero vegades:\n");
+   writeTxt(3, 2, "Temps:\n");
+   writeTxt(5, 2, "Velocitat:\n");
 }
 
 show_centrifugat() {
-   writeTxt(1, 0, "Numero vegades:\n");
-   writeTxt(3, 0, "Temps:\n");
-   writeTxt(5, 0, "Velocitat:\n");
-   writeTxt(7, 0, "OK = START");
+   writeTxt(1, 2, "Numero vegades:\n");
+   writeTxt(3, 2, "Temps:\n");
+   writeTxt(5, 2, "Velocitat:\n");
+   writeTxt(7, 2, "OK = START");
+}
+
+update_arrow() {
+   
 }
 
 
@@ -68,9 +92,12 @@ void main(void)
 
    __delay_ms(1000);       // Esperem 1 segon
    clearGLCD(0,7,0,127);   // Tornem a esborrar per al bucle principal
+   writeTxt(1, 0, ">");
+   unsigned char prev_buttons = 0;
    
    while (1)
    {  
+      unsigned 
       show_rentat();
       __delay_ms(10); // Pausa per evitar pampallugues excessives
    }
