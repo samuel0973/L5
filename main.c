ee64be43be27dd75fd854f63fa106c307686e92f
samuel0973 <samuel.bru.mezquita@estudiantat.upc.edu>
2026-05-03 19:36:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "myFunctions.h"
 
-
-void main() {
+void main(void) {
     unsigned char btn;
 
     init_pic();
@@ -12,9 +11,49 @@ void main() {
 
     while (1) {
         btn = debounce_buttons();
+        byte old_arrow_page = get_page(selected_param);
 
-        if (btn) {
-            handle_buttons(btn);
+        if ((btn & BTN_RIGHT) && selected_screen != CENTRIFUGAT) {
+            my_clear(selected_screen);
+            selected_screen = (selected_screen + 1) % NUM_SCREENS;
+            selected_param = PARAM_0;
+            draw_screen(selected_screen);
+        }
+
+        if ((btn & BTN_LEFT) && selected_screen != RENTAT) {
+            my_clear(selected_screen);
+            selected_screen = (selected_screen - 1 + NUM_SCREENS) % NUM_SCREENS;
+            selected_param = PARAM_0;
+            draw_screen(selected_screen);
+        }
+
+        if (btn & BTN_OK) {
+            if (edit_mode == 0)
+                edit_mode = 1;
+            else
+                edit_mode = 0;
+
+            draw_arrow(selected_param, old_arrow_page);
+        }
+
+        if (btn & BTN_UP) {
+            if (edit_mode) {
+                increase_value(selected_screen, selected_param);
+                draw_edit(selected_param);
+            } else if (selected_param != PARAM_0) {
+                selected_param = (selected_param - 1 + NUM_PARAMS) % NUM_PARAMS;
+                draw_arrow(selected_param, old_arrow_page);
+            }
+        }
+
+        if (btn & BTN_DOWN) {
+            if (edit_mode) {
+                decrease_value(selected_screen, selected_param);
+                draw_edit(selected_param);
+            } else if (selected_param != PARAM_SPEED) {
+                selected_param = (selected_param + 1) % NUM_PARAMS;
+                draw_arrow(selected_param, old_arrow_page);
+            }
         }
     }
 }
